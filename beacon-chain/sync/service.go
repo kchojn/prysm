d11df8f26cdad0eb478a919704524e623e4ff1e2
Potuz <potuz@prysmaticlabs.com>
2024-08-29 09:38:18 -0300

    Receive ptc message (#14394)
    * Handle incoming ptc attestation messages in the chain package
    
    * fix double import
    
    * remove unused error

--- beacon-chain/sync/service.go
+++ beacon-chain/sync/service.go
@@ -107,6 +107,7 @@ type config struct {
 // This defines the interface for interacting with block chain service
 type blockchainService interface {
 	blockchain.BlockReceiver
+	blockchain.PayloadAttestationReceiver
 	blockchain.BlobReceiver
 	blockchain.HeadFetcher
 	blockchain.FinalizationFetcher
